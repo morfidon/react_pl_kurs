1e6869d674fae8f0980945933665be73f1fe464d
morfidon <armiksos@gmail.com>
2024-08-21 18:19:49 +0200

    ClickPower

--- src/components/GoldClicker.jsx
+++ src/components/GoldClicker.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react';
 function GoldClicker() 
 {
     const [gold, setGold] = useState(0);
+    const [clickPower, setClickPower] = useState(1);
+    const [clickUpgradeCost, setClickUpgradeCost] = useState(10);
+
+    const upgradeClickPower = () => {
+        if(gold >= clickUpgradeCost)
+        {
+            setGold(gold - clickUpgradeCost);
+            setClickPower(clickPower + 1);
+            setClickUpgradeCost(clickUpgradeCost * 2);
+        }
+    }
     return(
         <div>
             <h1>Gold Clicker</h1>
             <p>Gold: {gold}</p>
-            <button onClick={() => setGold(gold + 1)}>Dig gold</button>
+            <p>Click power: {clickPower}</p>
+            <button onClick={() => setGold(gold + clickPower)}>Dig gold</button>
+            <button onClick={upgradeClickPower}>Upgrade click power (Cost: {clickUpgradeCost})</button>
 
         </div>
     );
