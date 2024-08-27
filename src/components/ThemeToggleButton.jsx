a4eed8e9b03ecfc071d142eda1aa61fcf2824170
morfidon <armiksos@gmail.com>
2024-08-27 12:22:56 +0200

    toggle dev

--- src/components/ThemeToggleButton.jsx
+++ src/components/ThemeToggleButton.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import styles from './ThemeToggleButton.module.css'
+import "@theme-toggles/react/css/Classic.css"
+import { Classic } from "@theme-toggles/react"
 
 function ThemeToggleButton()
 {
@@ -18,7 +20,19 @@ function ThemeToggleButton()
             >
                 {theme === 'light' ? 'Dark' : 'Light'}
             </button>
-       
+
+            <Classic duration={750} 
+                     style={
+                        {
+                            color: theme === 'light' ? 'blue' : 'yellow',
+                            backgroundColor: "var(--background-color)",
+                            fontSize: "1.6em",
+
+                        }
+                     }
+                     toggled={theme === 'light'}
+                     toggle={() => setTheme(theme === 'light' ? 'dark' : 'light')}  
+            />
         </>
     );
 
