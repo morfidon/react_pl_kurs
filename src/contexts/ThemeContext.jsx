ebacfabe40f0a8b33e81c3e383e11aa040e3608a
morfidon <armiksos@gmail.com>
2024-08-27 17:16:53 +0200

    Reapply "localst"
    This reverts commit ab7a4cf92cf89270989a20e69c0e3fccb1a5900f.

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,12 +1,18 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect } from 'react';
+import useLocalStorageState from 'use-local-storage-state'
 
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useLocalStorageState('theme',
+        {
+            defaultValue: 'light',
+        }
+    );
 
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
+
     }, [theme]);  
 
     return (
