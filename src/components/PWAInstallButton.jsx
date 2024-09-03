6ebb9c81ba77339e96a4b431d6bb00d0e69756b1
morfidon <armiksos@gmail.com>
2024-09-03 15:32:46 +0200

    component visibility

--- src/components/PWAInstallButton.jsx
+++ src/components/PWAInstallButton.jsx
@@ -1,11 +1,22 @@
-
+import { useState } from 'react'
 function PWAInstallButton() {
-
-    return (
+    const [showInstallButton, setShowInstallButton] = useState(false);
+    
+    // return (  
+    //     showInstallButton ?      
+    //     <button>
+    //         Install PWA app
+    //     </button>
+    //     : null
+    // )
+    return (  
+        showInstallButton 
+        &&      
         <button>
             Install PWA app
         </button>
-    )
+       
+    )    
 }
 
 export default PWAInstallButton
