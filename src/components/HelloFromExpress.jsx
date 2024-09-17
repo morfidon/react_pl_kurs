fca14efd9187be46b3f3e4dac94246256f6683d4
morfidon <armiksos@gmail.com>
2024-09-17 13:38:47 +0200

    react spinner

--- src/components/HelloFromExpress.jsx
+++ src/components/HelloFromExpress.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { RotatingLines } from 'react-loader-spinner'
 function HelloFromExpress() 
 {
     const [fetchMessage, setFetchMessage] = useState('');
@@ -15,7 +15,17 @@ function HelloFromExpress()
 
         fetchMessage();
     }, []);
-    return ( <h1>{fetchMessage ? fetchMessage : 'Loading...'}</h1> );
+    return ( <h1>{fetchMessage ? fetchMessage : <RotatingLines
+        visible={true}
+        height="48"
+        width="48"
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        />}</h1> );
 }
 
 export default HelloFromExpress;
