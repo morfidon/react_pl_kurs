647203b76349d096a5dc2cd0a8fbec47c84e2a95
morfidon <armiksos@gmail.com>
2024-10-21 14:51:56 +0200

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,25 +1,38 @@
+import { useState } from 'react'
 import { v4 } from 'uuid'
 function TaskList(){
     const tasks = [
-        'Do homework',
-        'Buy milk',
-        'Clean room',
-        'Cook dinner'       
+        'Do homework', //100
+        'Buy milk', //79
+        'Clean room', //5125125
+        'Cook dinner' //12421  
     ]
-    const tasksProccessed = tasks.map(task => (
+    const [tasksDictionary, setTasksDictionary] = useState(tasks.map(task => (
         {
             id: v4(),
             text: task
         }
-    ))
+    )))
 
     // map - przyporządkuj
-    
+    // tasks.map(element_z_taska => co_ma_sie_stac_z_elemen_z_taska)
+    // tasks.filter(element_z_taska => warunek)
+    // Które elementy mają wartość 'true' dla tego warunku i te właśnie maja trafić do NOWEJ tablicy
+    const handleRemoveTask = (id) => //79
+    {
+        setTasksDictionary(tasksDictionary.filter(
+            task => task.id !== id
+        ))
+    };
     return (
         <>
            <ul>
                 {
-                    tasksProccessed.map(task => <li key={task.id}>{task.text}</li>)
+                    tasksDictionary.map(task => 
+                    <li key={task.id}>{task.text}
+                      <button onClick={() => handleRemoveTask(task.id)}>X</button>
+                    </li>
+                )
                 }
            </ul>
         </>     
